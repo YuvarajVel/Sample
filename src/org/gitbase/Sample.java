06072bc6784473d4faf52e43347f52d735378b3d
YuvarajVel <135580694+YuvarajVel@users.noreply.github.com>
2023-07-20 18:52:15 +0530

    New

---
+++ src/org/gitbase/Sample.java
@@ -0,0 +1,16 @@
+package org.gitbase;
+
+public class Sample {
+	
+	private void test() {
+		System.out.println("This is Yuvaraj Branch");
+
+	}
+	
+	public static void main(String[] args) {
+		Sample l= new Sample();
+		l.test();
+				
+	}
+
+}
